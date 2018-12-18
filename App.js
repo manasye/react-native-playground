4c4ed94e22021ddf705204f7cdf0a48ebaeb6bd0
manasyebukit <13516122@std.stei.itb.ac.id>
2018-12-18 11:27:24 +0700

    Restructuring project

--- App.js
+++ App.js
@@ -1,49 +1,28 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
-type Props = {};
-export default class App extends Component<Props> {
+import PlaceInput from './src/components/PlaceInput/PlaceInput';
+import PlaceList from './src/components/PlaceList/PlaceList';
+
+export default class App extends Component {
     state = {
-        placeName: '',
         places: []
     };
 
-    placeNameChangeHandler = val => {
-        this.setState({ placeName: val });
-    };
-
-    placeSubmitHandler = () => {
-        if (this.state.placeName.trim() === '') {
-            return;
-        }
+    placeAddedHandler = placeName => {
         this.setState(prevState => {
             console.log(prevState);
             return {
-                places: prevState.places.concat(prevState.placeName)
+                places: prevState.places.concat(placeName)
             };
         });
     };
 
     render() {
-        const placesOutput = this.state.places.map((place, idx) => (
-            <Text key={idx}>{place}</Text>
-        ));
         return (
             <View style={styles.container}>
-                <View style={styles.inputContainer}>
-                    <TextInput
-                        style={styles.placeInput}
-                        value={this.state.placeName}
-                        placeholder="Awesome"
-                        onChangeText={this.placeNameChangeHandler}
-                    />
-                    <Button
-                        title="Add"
-                        style={styles.placeButton}
-                        onPress={this.placeSubmitHandler}
-                    />
-                </View>
-                <View>{placesOutput}</View>
+                <PlaceInput onPlaceAdded={this.placeAddedHandler} />
+                <PlaceList places={this.state.places} />
             </View>
         );
     }
@@ -52,22 +31,9 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'flex-start',
+        padding: 26,
+        backgroundColor: '#fff',
         alignItems: 'center',
-        backgroundColor: '#FFF',
-        padding: 25
-    },
-    inputContainer: {
-        // flex: 1,
-        width: '100%',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center'
-    },
-    placeInput: {
-        width: '70%'
-    },
-    placeButton: {
-        width: '30%'
+        justifyContent: 'flex-start'
     }
 });
